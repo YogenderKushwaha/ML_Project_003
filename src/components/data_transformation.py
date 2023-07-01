88372bec59dfb4a296bfab6ae17c5724e65d5fa2
Yogender Kushwaha <ykushwaha20@gmail.com>
2023-07-01 21:27:15 +0530

    Model training Done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -49,8 +49,8 @@ class DataTransformation:
                 ]
             )
 
-            logging.info("Categorical Columns: ",categorical_columns)
-            logging.info("Numerical Columns: ",numerical_columns)
+            logging.info(f"Categorical Columns:{categorical_columns} ")
+            logging.info(f"Numerical Columns: {numerical_columns} ")
 
             preprocessor = ColumnTransformer(
                 [
