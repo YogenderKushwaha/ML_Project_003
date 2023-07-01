706a1626db4de0fe750f35e282be653d69d197f1
Yogender Kushwaha <ykushwaha20@gmail.com>
2023-07-01 19:40:05 +0530

    Data transformation completed

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+import pickle
+
+def save_obj(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok = True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+
+
+    except Exception as e:
+        logging.info("Error Occured while Saving the pickle file")
+        raise CustomException(e, sys)
